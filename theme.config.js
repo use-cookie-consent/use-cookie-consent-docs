908f4963e8bc9802befa6e8ab872c26522350146
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2021-11-23 20:21:56 +0200

    Initial commit

---
+++ theme.config.js
@@ -0,0 +1,28 @@
+export default {
+  projectLink: "https://github.com/shuding/nextra", // GitHub link in the navbar
+  docsRepositoryBase: "https://github.com/shuding/nextra/blob/master", // base URL for the docs repository
+  titleSuffix: " – useCookieConsent",
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Antoni Silvestrovič.`,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <span style={{ marginRight: "12px", fontSize: "36px" }}>🍪</span>
+      <span style={{ fontWeight: 500, fontSize: "21px" }}>
+        useCookieConsent
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Headless cookie consent management" />
+      <meta name="og:title" content="useCookieConsent JavaScript hook" />
+    </>
+  ),
+};

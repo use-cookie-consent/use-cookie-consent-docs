dfa6cb6b63eef4082418d1600901d505e328e611
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2022-07-30 22:14:15 +0300

    Updated config

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const KNOWN_LANGUAGES = {
 };
 
 // Uncomment this to add an "Edit this page" button to every page of documentation.
-export const GITHUB_EDIT_URL = `https://github.com/use-cookie-consent/use-cookie-consent-docs/blob/main/docs/`;
+export const GITHUB_EDIT_URL = `https://github.com/use-cookie-consent/use-cookie-consent-docs/blob/main/`;
 
 // Uncomment this to add an "Join our Community" button to every page of documentation.
 export const COMMUNITY_INVITE_URL = `https://discord.gg/pa8epvzJbb`;
